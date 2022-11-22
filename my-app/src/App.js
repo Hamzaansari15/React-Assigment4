2eed74e0f2fb7f7a05145b68ce6151920320cc1b
Hamzaansari <hamzaansaree15@gmail.com>
2022-11-23 03:52:37 +0500

    Adding Second Slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/header/header';
+import Body from './components/body/body';
 function App() {
   return (
     <div >
       <Header/>
+      <Body/>
     </div>
   );
 }
---
+++ my-app/src/components/body/body.js
@@ -0,0 +1,11 @@
+import Heading from "./heading/heading";
+import SwipeToSlide from "./slider/slider";
+const Body = () => {
+    return(
+        <div>
+            <Heading/>
+            <SwipeToSlide/> 
+        </div>
+    )
+}
+export default Body;
