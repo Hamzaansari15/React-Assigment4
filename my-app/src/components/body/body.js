2786e04072a7fd1a43450540a0e3d71ca7eba9d8
Hamzaansari <hamzaansaree15@gmail.com>
2022-11-24 05:55:38 +0500

    Second Slider Completed

--- my-app/src/components/body/body.js
+++ my-app/src/components/body/body.js
@@ -1,10 +1,12 @@
 import Heading from "./heading/heading";
 import SwipeToSlide from "./slider/slider";
+import SecondSliderItem from "./slider/secondslideritem";
 const Body = () => {
     return(
         <div>
             <Heading/>
             <SwipeToSlide/> 
+            <SecondSliderItem/>
         </div>
     )
 }
