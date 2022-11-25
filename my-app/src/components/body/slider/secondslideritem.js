a3e57e7573b20c368d3ced2a8c0deb04ada83ab5
Hamzaansari <hamzaansaree15@gmail.com>
2022-11-26 04:25:58 +0500

    Second Slider Completed

--- my-app/src/components/body/slider/secondslideritem.js
+++ my-app/src/components/body/slider/secondslideritem.js
@@ -1,105 +1,75 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './slider.css';
-import Slider from "react-slick";
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
-const SecondSliderItem = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        lazyLoad: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+
+
+/* https://resources.turo.com/f/81934/416x284/6e085cbe0e/image_outdoors_416x285.jpg 
+       https://resources.turo.com/f/81934/416x284/7c424a07d5/image_unwind_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/929b21c4fa/image_upgrade_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/1e828ae782/image_scenic_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/a3870434ac/image_try_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/fa43ee8e2b/image_errand_416x285.jpg
+  
+    */
+
+function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
     };
+
     return (
-        <>
-            <div className="main_slider2">
-                <div className="slider2">
-                    <Slider {...settings}>
-                        <div>
-                            <div id="slider2" className="slider_section">
-                                <div className="slider_image2">
-                                    <img src="https://resources.turo.com/f/81934/416x284/6e085cbe0e/image_outdoors_416x285.jpg" />
-                                </div>
-                                <div className="slider_box2">
-                                    <div className="slider_heading2">Find the perfect car
-                                        <span> to the great outdoors</span>
-                                    </div>
-                                    <div className="slider_text2">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</div>
-                                    <button className="button">Browse cars</button>                                
-                                    </div>
-                            </div>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <div className="main_slider2">
+                    <div className="slider2">
+                        <div className="slider_image2">
+                            <img
+                                className="d-block"
+                                src=" https://resources.turo.com/f/81934/416x284/6e085cbe0e/image_outdoors_416x285.jpg "
+                                alt="First slide" />
                         </div>
-                        <div>
-                            <div id="slider2" className="slider_section">
-                                <div className='slider_image2'>
-                                    <img src='https://resources.turo.com/f/81934/416x284/7c424a07d5/image_unwind_416x285.jpg' />
-                                </div>
-                                <div className="slider_box2">
-                                    <div className="slider_heading2">Find the perfect car
-                                        <span> to unwind for the weekend</span>
-                                    </div>
-                                    <div className="slider_text2">From daily drivers to spirited sports cars, ditch the grind with convenient nearby cars.</div>
-                                    <button className="button">Browse cars</button>                                </div>
-                            </div>
+                        <div className="slider_caption">
+                            <h1 className="slider_heading2">Find the perfect car
+                                <span> to conquer the great outdoors</span>
+                            </h1>
+                            <p className="slider_text2">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</p>
                         </div>
-                        <div>
-                            <div id="slider2" className="slider_section">
-                                <div className='slider_image2'>
-                                    <img src='https://resources.turo.com/f/81934/416x284/929b21c4fa/image_upgrade_416x285.jpg' />
-                                </div>
-                                <div className="slider_box2">
-                                    <div className="slider_heading2">Find the perfect car
-                                        <span> to your vacation plans</span>
-                                    </div>
-                                    <div className="slider_text2">Skip the rental car counter and find the perfect car to complement your vacation vibe.</div>
-                                    <button className="button">Browse cars</button>                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div id="slider2" className="slider_section">
-                                <div className='slider_image2'>
-                                    <img src='https://resources.turo.com/f/81934/416x284/1e828ae782/image_scenic_416x285.jpg' />
-                                </div>
-                                <div className="slider_box2">
-                                    <div className="slider_heading2">Find the perfect car
-                                        <span> for scenic corners & curves</span>
-                                    </div>
-                                    <div className="slider_text2">Get your fill of high-performance thrills, with the richest selection of luxury and exotic vehicles anywhere</div>
-                                    <button className="button">Browse cars</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div id="slider2" className="slider_section">
-                                <div className='slider_image2'>
-                                    <img src='https://resources.turo.com/f/81934/416x284/a3870434ac/image_try_416x285.jpg' />
-                                </div>
-                                <div className="slider_box2">
-                                    <div className="slider_heading2">Find the perfect car
-                                        <span> to try before you buy</span>
-                                    </div>
-                                    <div className="slider_text2">Make sure your future wheels work well with your lifestyle by taking your time in the driver’s seat.</div>
-                                    <button className="button">Browse cars</button>                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div id="slider2" className="slider_section">
-                                <div className='slider_image2'>
-                                    <img src='https://resources.turo.com/f/81934/416x284/fa43ee8e2b/image_errand_416x285.jpg' />
-                                </div>
-                                <div className="slider_box2">
-                                    <div className="slider_heading2">Find the perfect car
-                                        <span> to make errand day easier</span>
-                                    </div>
-                                    <div className="slider_text2">Book a roomy truck or SUV to ease your errand day stress, or to motivate your moving day.</div>
-                                    <button className="button">Browse cars</button>                                </div>
-                            </div>
-                        </div>
-                    </Slider></div>
-            </div>
-        </>
-    )
+                    </div>
+                </div>
+            {/* </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Second slide&bg=282c34"
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Third slide&bg=20232a"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption> */}
+            </Carousel.Item>
+        </Carousel>
+    );
 }
-export default SecondSliderItem;
+
+export default ControlledCarousel;
--- my-app/src/components/body/slider/slider.js
+++ my-app/src/components/body/slider/slider.js
@@ -1,111 +1,244 @@
 import Slider from "react-slick";
-import React, { Component } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import FirstSliderItem from "./firstsliderItem";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './slider.css';
-import  FirstSliderItem from "./firstsliderItem";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'swiper/css';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 
+const SwipeToSlide = () => {
 
-export default class SwipeToSlide extends Component {
-    render() {
-        const settings = {
-            rows: 1,
-            speed: 300,
-            slidesPerRow: 1,
-            slidesToShow: 5,
-            infinite: true,
-            swipeToSlide: true,
-            slidesToScroll: 1,
-            lazyLoad: true,
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 5,
-                        slidesToScroll: 1,
-                    }
-                },
-                {
-                    breakpoint: 1080,
-                    settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 1,
-                    }
-                },
-                {
-                    breakpoint: 880,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        arrows: true
-                    }
-                },
-                {
-                    breakpoint: 650,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        arrows: true
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        arrows: true
-                    }
+    const settings = {
+        rows: 1,
+        speed: 300,
+        slidesPerRow: 1,
+        slidesToShow: 5,
+        infinite: true,
+        swipeToSlide: true,
+        slidesToScroll: 1,
+        lazyLoad: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
                 }
-            ]
-
-        };
-        const imageArr = [
-            'https://resources.turo.com/f/81934/386x308/9bf274f19e/image_make_jeep-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/7dcf9bff19/image_make_tesla-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/ef857a4438/image_make_subaru-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/7e7ce60797/image_make_porsche-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/0c4f5f5186/image_make_bmw-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/065308f2c4/image_make_mb-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/2809ca8702/image_make_toyota-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/994b145776/image_make_nissan-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/a470b4f290/image_make_lambo-2x.jpg',
-            'https://resources.turo.com/f/81934/386x308/0431f873ac/image_make_ford-2x.jpg'
+            },
+            {
+                breakpoint: 1080,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 880,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true
+                }
+            }
         ]
-        const titleArr = [
-            'Jeep',
-            'Tesla',
-            'Subaru',
-            'Porsche',
-            'BMW',
-            'Mercedez-Benz',
-            'Toyota',
-            'Nissan',
-            'Lamborghini',
-            'Ford'
-        ]
-        return (
-            <>
-                <div className="main_slider">
-                    <div className="slider" >
-                        <h2 id="slider_heading">Browse by make</h2>
-                        <Slider {...settings}>
 
-                            <FirstSliderItem image={imageArr[0]} title={titleArr[0]} />
-                            <FirstSliderItem image={imageArr[1]} title={titleArr[1]} />
-                            <FirstSliderItem image={imageArr[2]} title={titleArr[2]} />
-                            <FirstSliderItem image={imageArr[3]} title={titleArr[3]} />
-                            <FirstSliderItem image={imageArr[4]} title={titleArr[4]} />
-                            <FirstSliderItem image={imageArr[5]} title={titleArr[5]} />
-                            <FirstSliderItem image={imageArr[6]} title={titleArr[6]} />
-                            <FirstSliderItem image={imageArr[7]} title={titleArr[7]} />
-                            <FirstSliderItem image={imageArr[8]} title={titleArr[8]} />
-                            <FirstSliderItem image={imageArr[9]} title={titleArr[9]} />
+    };
+    const imageArr = [
+        'https://resources.turo.com/f/81934/386x308/9bf274f19e/image_make_jeep-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/7dcf9bff19/image_make_tesla-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/ef857a4438/image_make_subaru-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/7e7ce60797/image_make_porsche-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/0c4f5f5186/image_make_bmw-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/065308f2c4/image_make_mb-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/2809ca8702/image_make_toyota-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/994b145776/image_make_nissan-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/a470b4f290/image_make_lambo-2x.jpg',
+        'https://resources.turo.com/f/81934/386x308/0431f873ac/image_make_ford-2x.jpg'
+    ]
+    const titleArr = [
+        'Jeep',
+        'Tesla',
+        'Subaru',
+        'Porsche',
+        'BMW',
+        'Mercedez-Benz',
+        'Toyota',
+        'Nissan',
+        'Lamborghini',
+        'Ford'
+    ]
+    return (
+        <>
+            <div className="main_slider">
+                <div className="slider" >
+                    <h2 id="slider_heading">Browse by make</h2>
+                    <Slider {...settings}>
 
-                        </Slider>
-                    </div>
+                        <FirstSliderItem image={imageArr[0]} title={titleArr[0]} />
+                        <FirstSliderItem image={imageArr[1]} title={titleArr[1]} />
+                        <FirstSliderItem image={imageArr[2]} title={titleArr[2]} />
+                        <FirstSliderItem image={imageArr[3]} title={titleArr[3]} />
+                        <FirstSliderItem image={imageArr[4]} title={titleArr[4]} />
+                        <FirstSliderItem image={imageArr[5]} title={titleArr[5]} />
+                        <FirstSliderItem image={imageArr[6]} title={titleArr[6]} />
+                        <FirstSliderItem image={imageArr[7]} title={titleArr[7]} />
+                        <FirstSliderItem image={imageArr[8]} title={titleArr[8]} />
+                        <FirstSliderItem image={imageArr[9]} title={titleArr[9]} />
+
+                    </Slider>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
+
 }
+const SecondSlider = () => {
+    return (
+        <>
+            <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={false}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="main_slider2 mx-5">
+                        <div className="slider2">
+                            <div className="slider_image2 mx-3 mb-3">
+                                <img
+                                    className="d-block"
+                                    src=" https://resources.turo.com/f/81934/416x284/6e085cbe0e/image_outdoors_416x285.jpg "
+                                    alt="First slide" />
+                            </div>
+                            <div className="slider_caption mx-3 mb-3">
+                                <h2 className="slider_heading2">Find the perfect car
+                                    <span> to conquer the great outdoors</span>
+                                </h2>
+                                <p className="slider_text2 mt-3">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</p>
+                            <div className="mt-5"><button className="p-2 px-3 slider_btn">Browse car</button></div>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide><div className="main_slider2 mx-5">
+                        <div className="slider2">
+                            <div className="slider_image2 mx-3 mb-3">
+                                <img
+                                    className="d-block"
+                                    src=" https://resources.turo.com/f/81934/416x284/7c424a07d5/image_unwind_416x285.jpg "
+                                    alt="First slide" />
+                            </div>
+                            <div className="slider_caption mx-3 mb-3">
+                                <h2 className="slider_heading2">Find the perfect car
+                                    <span> to conquer the great outdoors</span>
+                                </h2>
+                                <p className="slider_text2 mt-3">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</p>
+                            <div className="mt-5"><button className="p-2 px-3 slider_btn">Browse car</button></div>
+                            </div>
+                        </div>
+                    </div></SwiperSlide>
+                <SwiperSlide><div className="main_slider2 mx-5">
+                        <div className="slider2">
+                            <div className="slider_image2 mx-3 mb-3">
+                                <img
+                                    className="d-block"
+                                    src="https://resources.turo.com/f/81934/416x284/929b21c4fa/image_upgrade_416x285.jpg"
+                                    alt="First slide" />
+                            </div>
+                            <div className="slider_caption mx-3 mb-3">
+                                <h2 className="slider_heading2">Find the perfect car
+                                    <span> to conquer the great outdoors</span>
+                                </h2>
+                                <p className="slider_text2 mt-3">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</p>
+                            <div className="mt-5"><button className="p-2 px-3 slider_btn">Browse car</button></div>
+                            </div>
+                        </div>
+                    </div></SwiperSlide>
+                <SwiperSlide><div className="main_slider2 mx-5">
+                        <div className="slider2">
+                            <div className="slider_image2 mx-3 mb-3">
+                                <img
+                                    className="d-block"
+                                    src=" https://resources.turo.com/f/81934/416x284/1e828ae782/image_scenic_416x285.jpg "
+                                    alt="First slide" />
+                            </div>
+                            <div className="slider_caption mx-3 mb-3">
+                                <h2 className="slider_heading2">Find the perfect car
+                                    <span> to conquer the great outdoors</span>
+                                </h2>
+                                <p className="slider_text2 mt-3">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</p>
+                            <div className="mt-5"><button className="p-2 px-3 slider_btn">Browse car</button></div>
+                            </div>
+                        </div>
+                    </div></SwiperSlide>
+                <SwiperSlide><div className="main_slider2 mx-5">
+                        <div className="slider2">
+                            <div className="slider_image2 mx-3 mb-3">
+                                <img
+                                    className="d-block"
+                                    src="https://resources.turo.com/f/81934/416x284/a3870434ac/image_try_416x285.jpg "
+                                    alt="First slide" />
+                            </div>
+                            <div className="slider_caption mx-3 mb-3">
+                                <h2 className="slider_heading2">Find the perfect car
+                                    <span> to conquer the great outdoors</span>
+                                </h2>
+                                <p className="slider_text2 mt-3">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</p>
+                            <div className="mt-5"><button className="p-2 px-3 slider_btn">Browse car</button></div>
+                            </div>
+                        </div>
+                    </div></SwiperSlide>
+                <SwiperSlide><div className="main_slider2 mx-5">
+                        <div className="slider2">
+                            <div className="slider_image2 mx-3 mb-3">
+                                <img
+                                    className="d-block"
+                                    src=" https://resources.turo.com/f/81934/416x284/fa43ee8e2b/image_errand_416x285.jpg "
+                                    alt="First slide" />
+                            </div>
+                            <div className="slider_caption mx-3 mb-3">
+                                <h2 className="slider_heading2">Find the perfect car
+                                    <span> to conquer the great outdoors</span>
+                                </h2>
+                                <p className="slider_text2 mt-3">Go prepared in a rugged 4x4 to take on winter roads with ease, or a camper van to take you to the trees.</p>
+                            <div className="mt-5"><button className="p-2 px-3 slider_btn">Browse car</button></div>
+                            </div>
+                        </div>
+                    </div></SwiperSlide>
+            </Swiper>
+        </>
+    );
+}
+/* https://resources.turo.com/f/81934/416x284/6e085cbe0e/image_outdoors_416x285.jpg 
+       https://resources.turo.com/f/81934/416x284/7c424a07d5/image_unwind_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/929b21c4fa/image_upgrade_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/1e828ae782/image_scenic_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/a3870434ac/image_try_416x285.jpg
+       https://resources.turo.com/f/81934/416x284/fa43ee8e2b/image_errand_416x285.jpg
+  
+    */
+export { SwipeToSlide, SecondSlider };
