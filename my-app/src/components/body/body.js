75df11e248013fa1b0e286068b6340ef5cdecf07
Hamzaansari <hamzaansaree15@gmail.com>
2022-11-27 06:02:56 +0500

    Adding Forth Slider

--- my-app/src/components/body/body.js
+++ my-app/src/components/body/body.js
@@ -1,13 +1,20 @@
-import Heading from "./heading/heading";
-import {FirstSlider, SecondSlider, ThirdSlider} from "./slider/slider";
+import {Heading, SecondHeading, ImageSection} from "./heading/heading";
+import {FirstSlider, SecondSlider, ThirdSlider, FourthSlider} from "./slider/slider";
 
 const Body = () => {
+ 
+    let headingArr = ['Find your drive'];
+    let smallHeadingArr = ["Explore the world's largest car sharing marketplace"]
+ 
     return(
         <div>
-            <Heading/>
+            <Heading heading={headingArr[0]} smallHeading={smallHeadingArr[0]} />
             <FirstSlider/> 
             <SecondSlider/>
             <ThirdSlider/>
+            <SecondHeading/>
+            <ImageSection/>
+            <FourthSlider/>
         </div>
     )
 }
--- my-app/src/components/body/slider/firstsliderItem.js
+++ my-app/src/components/body/slider/firstsliderItem.js
@@ -6,7 +6,7 @@ import './slider.css';
 
 const FirstSliderItem = (props) => {
     return (
-        <a href="#" className='slider_box'>
+        <a className='slider_box'>
             <div className='slider_image'>
                 <img src={props.image} />
                 <h6 className="slider_text">{props.title}</h6>
