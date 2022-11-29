f80af4e2ccee887621214ea9536019ba897dd351
Hamzaansari <hamzaansaree15@gmail.com>
2022-11-30 03:27:10 +0500

    Assignment Completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header/header';
 import Body from './components/body/body';
+import Footer  from './components/footer/footer';
 function App() {
   return (
     <div >
       <Header/>
       <Body/>
+      <Footer/>
     </div>
   );
 }
