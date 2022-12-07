fd9507acb625cc16ad1d7028270d47775f623576
Hamzaansari <hamzaansaree15@gmail.com>
2022-12-08 02:18:30 +0500

    Update First Slider

--- my-app/src/components/body/slider/firstsliderItem.js
+++ my-app/src/components/body/slider/firstsliderItem.js
@@ -1,8 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './slider.css';
-import { useRef } from "react";
-
 
 
 const FirstSliderItem = (props) => {
